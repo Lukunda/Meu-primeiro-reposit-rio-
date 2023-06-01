e7d097d1c7269d7a4728d0f9e6957b87c574458a
Lukunda <brunetti.lucas96@gmail.com>
2023-06-01 11:31:28 -0300

    subindo novos arquivos

---
+++ scripts - Python/teste02.py
@@ -0,0 +1,7 @@
+Dia=input("Dia =")
+Mes=input("Mes =")
+Ano=input("Ano =")
+print("Você nasceu no " + Dia, "de", + Mes, "de", + Ano, ".Correto?")
+
+
+
---
+++ scripts - Python/teste04.py
@@ -0,0 +1,7 @@
+item = input("item = ")
+peso = input("peso = ")
+preço = input("preço =")
+print("Voce comprou" , peso, "gramas", "de", item, "total", preço, "reais")
+
+
+
